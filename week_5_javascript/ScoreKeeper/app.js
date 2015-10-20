58314a10281c5e185b6b1cdc65e232b98440962b
Dina <dina@imgix.com>
2015-10-19 18:36:32 -0700

    Final project proposal
    final project idea

--- week_5_javascript/ScoreKeeper/app.js
+++ week_5_javascript/ScoreKeeper/app.js
@@ -13,3 +13,16 @@ buttons.addEventListener("click", function() {
 	score = score - 5;
         document.getElementById("score").innerHTML = score + " Points";
     });
+
+// added this part in class, didn't do it on my own
+
+document.querySelector("#submit-custom-score")
+.addEventListener("click", function(){
+	var textScore = document
+	document.querySelector("#custom-score").value;
+	var numScore = parseInt(textScore);
+	score = numScore;
+
+	document.getElementById("score").innerHTML = score + " Points";
+
+});
